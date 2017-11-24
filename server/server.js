5341a39de07d0bac229d07cfe797046ea68ced7b
Pavan <pavandeshpande60@gmail.com>
2017-11-24 20:17:46 +0530

    Add new nessage and create message events

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '../public');
@@ -8,10 +10,39 @@ const publicPath = path.join(__dirname, '../public');
 
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-    console.log(`Started server on port ${port}.`);
+io.on('connection', (socket) => {
+    console.log('New User Connected.');
+
+    // socket.emit('newEmail', {
+    //     from: 'pavan@gmai.com',
+    //     text: 'hey whats up',
+    //     createdAt: new Date()
+    // });
+
+    // socket.on('createEmail', (newEmail) => {
+    //     console.log("Create Email:\n", newEmail);
+    // });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('New Message',newMessage);
+    });
+
+    socket.emit('newMessage',{
+        from:"pavan",
+        text:"Hey, man",
+        createdAt:new Date()
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected.');
+    });
 });
 
-
+server.listen(port, () => {
+    console.log(`Started server on port ${port}.`);
+});
